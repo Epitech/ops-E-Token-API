dff2c1d1a365c886a908c30b01639a469d328970
Mickael Leclerc <mickael.leclerc@teads.tv>
2019-09-14 03:15:54 +0200

    continue

--- api/database/handler.js
+++ api/database/handler.js
@@ -9,8 +9,35 @@
 'use strict';
 
 const database = require('./connection');
+let databaseConnected = false
+let promises = [];
 
 module.exports = function (sql, replacements, type, accept = null, reject = null) {
+    if (!databaseConnected) {
+        database
+            .authenticate()
+            .then(() => {
+                // Execute all .sql files
+                fs.readdir("./sql/", (err, files) => {
+                    files.forEach(file => {
+                        fs.readFile("./sql/" + file, (err, sql) => {
+                            promises.push(new Promise(resolve => {
+                                database.query(sql.toString()).then(() => {
+                                    resolve();
+                                });
+                            }));
+                        });
+                    });
+                });
+            })
+            .catch(err => {
+                console.error("Unable to connect to the database:", err);
+                process.exit(1);
+            });
+        databaseConnected = true;
+    }
+
+    Promise.all(promises);
     database.query(sql, {replacements, type})
         .then(function (result) {
             if (accept !== null) {
